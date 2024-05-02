2eff7f85f0af85759d9cb661d5944d5be7b0b8d6
ColtenOuO <jun930436@gmail.com>
2024-05-02 13:50:33 +0800

    hello world

---
+++ demo/src/main/java/com/example/demo/MainController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class MainController {
+    @RequestMapping("/MainPage")
+    public String index(@RequestParam(value="title", required=false, defaultValue="") String title, Model model) {
+        model.addAttribute("name", title);
+        return "index";  // src/main/resources/templates/index.html
+    }
+}
